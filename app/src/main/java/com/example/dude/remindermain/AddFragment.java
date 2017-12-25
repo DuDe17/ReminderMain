86a12869b15ba80ae69e1e767c8f607d6a95f5ff
DuDe17 <shuklaasit@gmail.com>
2017-12-26 03:33:55 +0530

    Add fragment layout

--- app/src/main/java/com/example/dude/remindermain/AddFragment.java
+++ app/src/main/java/com/example/dude/remindermain/AddFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 
 /**
@@ -23,7 +25,32 @@ public class AddFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View addFragment = inflater.inflate(R.layout.fragment_add, container, false);
+        final Spinner frequencySpinner = (Spinner) addFragment.findViewById(R.id.frequencySpinner);
+        final Spinner weekDaySpinner = (Spinner) addFragment.findViewById(R.id.weeekdaySpinner);
+        frequencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                final String fSrt = (String) frequencySpinner.getItemAtPosition(i);
+                if(fSrt.equalsIgnoreCase("Weekly"))
+                {
+                    weekDaySpinner.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    weekDaySpinner.setVisibility(View.GONE);
+                }
+                return;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+        return addFragment;
     }
 
+    public void addTask(View view) {
+    }
 }
